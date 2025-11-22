dfcd67e56ac68c3ade4a4e7e408575f23a43d0a4
Riddhima Bhanja <164170235+riddhimaBhanja@users.noreply.github.com>
2025-11-22 12:31:46 +0530

    Delete 23Nov/MongoSpring directory

--- 23Nov/MongoSpring/src/main/java/com/mongospring/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mongospring.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.mongospring.model.Student;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-}
