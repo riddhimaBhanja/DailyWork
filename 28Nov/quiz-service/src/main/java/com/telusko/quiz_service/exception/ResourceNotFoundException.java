aea9ed42af434cc2b3b59b360ea9d4b9d92adf38
Riddhima Bhanja <164170235+riddhimaBhanja@users.noreply.github.com>
2025-11-28 15:16:10 +0530

    Delete 28Nov/quiz-service directory

--- 28Nov/quiz-service/src/main/java/com/telusko/quiz_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.telusko.quiz_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
